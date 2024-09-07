7159dbabb712b874730c52ba1bf469166117c20d
DV-Fansler <dfansler@dv-fansler.com>
2024-09-06 20:55:51 -0400

    Create BSP.c

---
+++ BSP.c
@@ -0,0 +1,13 @@
+float ADC_Value;
+float ADC_Read(int channel_num);
+int fd_adc;
+double val;
+char AD_Buf[13];
+char readBuf[2];
+float myfloat;
+int percent;
+int adc_address = 0x48;
+int channel_number;
+int Run_Value;
+
+
